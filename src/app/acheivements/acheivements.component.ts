5c8694d886cd8aa4d6b3dc2ca9e8849df11aae88
Antonyrahul <antonyrahul96@gmail.com>
2020-06-12 01:39:59 +0530

    UX added

--- src/app/acheivements/acheivements.component.ts
+++ src/app/acheivements/acheivements.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-acheivements',
   templateUrl: './acheivements.component.html',
-  styleUrls: ['./acheivements.component.css']
+  styleUrls: ['./acheivements.component.scss']
 })
 export class AcheivementsComponent implements OnInit {
 
